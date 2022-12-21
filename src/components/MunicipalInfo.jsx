9c53a2c8c74d43f88aa8473c7b0df370b07dde8b
renatoft89 <renatoft89@gmail.com>
2022-12-21 18:04:09 -0300

    feat: return infos

--- src/components/MunicipalInfo.jsx
+++ src/components/MunicipalInfo.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { useSelector } from 'react-redux';
 
 function MunicipalInfo() {
   const detailsRegion = useSelector(({ detailsMicroregion }) => detailsMicroregion);
   console.log(detailsRegion);
   return (
-    <div>
-      <h1>Informações da Região</h1>
+    <div className='details-region'>
+      <h1>Informações das Cidades e Distritos da Região</h1>
+      {
+        detailsRegion.map((region) => (
+          <Fragment key={region.id}>
+            <h3>A Cidade de {region.nome}</h3>
+            <p>Sua Microregião é: {region.municipio.microrregiao.nome}</p>
+            <p>Sua mesorregião é: {region.municipio.microrregiao.mesorregiao.nome}</p>
+            <p>Pertence ao estado: 
+              {region.municipio.microrregiao.mesorregiao.UF.nome} - {region.municipio.microrregiao.mesorregiao.UF.sigla} </p>
+          </Fragment>
+        ))
+      }
     </div>
   );
 }
